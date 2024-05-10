8c5305c0506e979e2b095eb0fee080305fe44f09
Rickyshowtime <eric.chautems@gmail.com>
2024-05-10 12:00:11 +0200

    fix: Deno functions

--- volumes/functions/hello/index.ts
+++ volumes/functions/hello/index.ts
@@ -2,43 +2,15 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.177.1/http/server.ts";
+import { serve } from "https://deno.land/std@0.177.1/http/server.ts"
 
-async function getMessage() {
-  const data = {
-    message: `Hello from Supabase Edge Functions!`,
-  };
+serve(async () => {
+  return new Response(
+      `"Hello from Edge Functions!"`,
+      { headers: { "Content-Type": "application/json" } },
+  )
+})
 
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json", Connection: "keep-alive" },
-  });
-}
-
-async function postMessage(req: Request) {
-  const { name }: reqPayload = await req.json();
-  const data = {
-    message: `Hello ${name} from Supabase Edge Functions!`,
-  };
-
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json", Connection: "keep-alive" },
-  });
-}
-
-serve(async (req: Request) => {
-  const { url, method } = req;
-
-  if (method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
-  }
-
-  // call relevant method based on method and id
-  switch (true) {
-    case method === "GET":
-      return getMessage();
-    case method === "POST":
-      return postMessage(req);
-    default:
-      return getMessage();
-  }
-});
+// To invoke:
+// curl 'http://localhost:<KONG_HTTP_PORT>/functions/v1/hello' \
+//   --header 'Authorization: Bearer <anon/service_role API key>'
